520c85b6dd7203874a681b078a746b2cb1813984
zishanx <zishanalikhan53@gmail.com>
2026-04-28 23:52:12 +0530

    just pushing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
-import express from 'express'
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = requrie('dotenv')
+
+dotenv.config()
 
 const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the server.")
+})
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000')
